415c865857d15704231d11567513105e305f57b1
jishnugopalan <jishnugopalan2000@gmail.com>
2023-03-21 23:54:33 +0530

    21/03

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:ecosentry/Dasboard.dart';
 import 'package:ecosentry/Login.dart';
+import 'package:ecosentry/Looogin.dart';
 import 'package:ecosentry/Search.dart';
 import 'package:ecosentry/Customer/MyProfile.dart';
 import 'package:ecosentry/UserRegistration.dart';
 import 'package:flutter/material.dart';
+
+import 'Registration.dart';
 
 void main() {
   runApp(const MyApps());
@@ -15,7 +18,11 @@ class MyApps extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: SignIn()
+      home: UserSignUp(),
+      routes: {
+        '/login':(context) =>const MyLogin(),
+        '/signup':(context)=>const UserSignUp()
+      },
   
     );
   }
